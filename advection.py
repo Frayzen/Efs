a8b12f57c9d47fe3babbad293d4abf6d7ddd982e
tim-pearson <timothy-edward.pearson@epita.fr>
2025-03-13 15:58:12 +0100

    wip: using temp buffers for advection

--- advection.py
+++ advection.py
@@ -9,7 +9,9 @@ from ui import draw_line
 
 def advect_velocities(grid: MacGrid):
     pre = np.sum(np.abs(grid.xgrid)) + np.sum(np.abs(grid.ygrid))
+    xtemp = grid.xgrid.copy()
 
+    ytemp = grid.ygrid.copy()
     for i in range(1, WIDTH - 1):
         for j in range(1, HEIGHT):
             pos = np.array([i, j + 0.5])
@@ -18,7 +20,9 @@ def advect_velocities(grid: MacGrid):
             new_pos[0] = clamp(new_pos[0], 0, WIDTH - 1)
             new_pos[1] = clamp(new_pos[1], 1, HEIGHT - 1)
             nv = grid.interpolate_velocity(new_pos)
-            grid.xgrid[i, j] = nv[0]
+
+            # grid.xgrid[i, j] = nv[0]
+            xtemp[i, j] = nv[0]
     for i in range(1, WIDTH):
         for j in range(1, HEIGHT - 1):
             pos = np.array([i + 0.5, j])
@@ -27,7 +31,10 @@ def advect_velocities(grid: MacGrid):
             new_pos[0] = clamp(new_pos[0], 0, WIDTH - 1)
             new_pos[1] = clamp(new_pos[1], 1, HEIGHT - 1)
             nv = grid.interpolate_velocity(new_pos)
-            grid.ygrid[i, j] = nv[1]
+            # grid.ygrid[i, j] = nv[1]
+            ytemp[i, j] = nv[1]
+    grid.xgrid = xtemp.copy()
+    grid.ygrid = ytemp.copy()
 
     if CONSERVATIVE_ADVECTION:
         aft = np.sum(np.abs(grid.xgrid)) + np.sum(np.abs(grid.ygrid))
@@ -38,6 +45,7 @@ def advect_velocities(grid: MacGrid):
 
 def advect_scalar(scalar_grid: ScalarGrid, velocity_grid: MacGrid):
     pre = np.sum(np.abs(scalar_grid.field))
+    ftemp = scalar_grid.field
 
     for i in range(WIDTH):
         for j in range(HEIGHT):
@@ -49,7 +57,8 @@ def advect_scalar(scalar_grid: ScalarGrid, velocity_grid: MacGrid):
             new_pos[1] = clamp(new_pos[1], 0, HEIGHT - 0.5)
             nv = scalar_grid.interpolate_scalar(new_pos)
 
-            scalar_grid.field[j, i] = nv
+            ftemp[j, i] = nv
+    scalar_grid.field = ftemp.copy()
 
     aft = np.sum(np.abs(scalar_grid.field))
     if CONSERVATIVE_SCALAR and aft:
